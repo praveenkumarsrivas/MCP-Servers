1da7ad9b899a5dc44f3b9c4c6b26a9a3f4c3a510
praveenkumarsrivas <pks101295@gmail.com>
2025-07-26 11:02:38 +0530

    Integrate MCP client and update main script to include ChatOpenAI and StdioServerParameters for enhanced functionality.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import asyncio
+from imaplib import Commands
 from dotenv import load_dotenv
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 import os
-
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+
+stdio_server_params = StdioServerParameters(
+    command = 'python',
+    args = ['/Users/praveensrivas/Documents/MCP/MCP-Servers/servers/math_server.py'],
+)
+
 
 async def main():
     print("Hello from mcp-servers!")
