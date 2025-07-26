554fe233b8ad619d26c8975352092346e84ee808
praveenkumarsrivas <pks101295@gmail.com>
2025-07-26 11:40:17 +0530

    Enhance main script to include asynchronous session handling and integrate AI message processing for dynamic user queries.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
+from email import message
 from imaplib import Commands
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
@@ -20,8 +22,23 @@ stdio_server_params = StdioServerParameters(
 )
 
 
+from pprint import pprint
+from langchain_core.messages import AIMessage
 async def main():
-    print("Hello from mcp-servers!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+
+            tools = await load_mcp_tools(session)
+            print("Loaded tools:", [t.name for t in tools])
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 52+2*3?")]})
+            for message in result['messages']:
+                if isinstance(message, AIMessage) and message.content:
+                    print("OUTPUT:", message.content)
+
+
     
 
 if __name__ == "__main__":
